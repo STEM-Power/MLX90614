8ca9f5313c7fc0839561cb3cf24ad5957ed8ba6d
STEM-Power <81220298+STEM-Power@users.noreply.github.com>
2021-05-02 19:42:11 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -9,8 +9,9 @@
  */
 //% weight=100 color=#0975ff icon="\uf2c9" block="MLX90614"
 namespace MLX90614 {
- let ToData = pins.createBuffer(2);
-
+let ToData = pins.createBuffer(2);
+let receivedBuffer = pins.createBuffer(16);
+let irData = pins.createBuffer(1);
 /**
      * Setup MLX90614 module Tx Rx to micro:bit pins.
      * 設定MLX90614的Tx、Rx連接腳位
@@ -19,8 +20,7 @@ namespace MLX90614 {
     */
     //% weight=100
     //% blockId="setSerial" block="set TX to %pinTX | RX to %pinRX"
-    export function NFC_setSerial(pinTX: SerialPin, pinRX: SerialPin): void {
-
+    export function setSerial(pinTX: SerialPin, pinRX: SerialPin): void {
         serial.redirect(
             pinRX,
             pinTX,
@@ -30,26 +30,44 @@ namespace MLX90614 {
     }
 
 
-//% weight=80
+//% weight=90
     //% blockId="getTo" block="Object Temperature"
     export function getTo(): number {
         serial.setRxBufferSize(100)
-        let receivedBuffer: number[] = []
         let TocmdBuffer: [0x53, 0xB4, 0x02, 0x07, 0x53, 0xB5, 0x04, 0x50]
         let ToBuffer: number[] = []
         let cmdTo = pins.createBufferFromArray(TocmdBuffer)
         serial.writeBuffer(cmdTo);
         basic.pause(1);
-        receivedBuffer = serial.readBuffer(2);;
+        receivedBuffer = serial.readBuffer(8);;
             for (let i = 0; i < 2; i++) {
-                ToData[i] = receivedBuffer[i];
+                ToData[i] = receivedBuffer[0 + i];
             }
-        ToBuffer = [ToData[0], ToData[1]];
+
         let to = (ToData[1]<<8 | ToData[0])
         to *= .02
         to -= 273.15
         return to
 	}
+
+//% weight=90
+    //% blockId="readRegister" block="Read internal register 0x06"
+    export function getIR(): number {
+        serial.setRxBufferSize(100)
+        let ircmdBuffer: [0x53, 0x06, 0x07, 0x08, 0x50]
+        let irBuffer: number[] = []
+        let cmdir = pins.createBufferFromArray(ircmdBuffer)
+        serial.writeBuffer(cmdir);
+        basic.pause(1);
+        receivedBuffer = serial.readBuffer(8);;
+            for (let i = 0; i < 3; i++) {
+                irData[i] = receivedBuffer[0 + i];
+            }
+
+        let ir = (irData[0])
+        return ir
+	}
+
 
 }
 
