0ffc367a923150e17ebb92daaa1d9d87317888ea
STEM-Power <81220298+STEM-Power@users.noreply.github.com>
2021-05-03 10:19:17 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -90,10 +90,11 @@ let irData = pins.createBuffer(1);
     //% blockId="readRegister" block="Read internal register 0x06"
     export function getIR(): number {
         serial.setRxBufferSize(100)
-        let serialBuffer = pins.createBuffer(3);
+        let serialBuffer = pins.createBuffer(4);
         serialBuffer[0] = 0x52
         serialBuffer[1] = 0x06
-        serialBuffer[2] = 0x50
+        serialBuffer[2] = 0x07
+        serialBuffer[3] = 0x50
         serial.writeBuffer(serialBuffer)
         basic.pause(1);
         receivedBuffer = serial.readBuffer(2);;
